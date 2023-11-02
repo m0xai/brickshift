daeccd273a623c1e01f63d6f06f260683f21f501
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-11-03 00:16:14 +0100

    feat: initialize frontend with quasar framework

---
+++ frontend/src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex flex-center">
+    <create-event-form />
+    <event-table />
+  </q-page>
+</template>
+
+<script setup>
+import { defineComponent } from "vue";
+import EventTable from "components/EventTable.vue";
+import CreateEventForm from "components/CreateEventForm.vue";
+
+defineComponent({name: "IndexPage"})
+
+</script>
