07928737767f236e9caeb3aca94e17a2375ac3b8
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-11-03 02:43:58 +0100

    feat(frontend): send basic POST request to create Events

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -2,20 +2,32 @@
   <q-page class="flex flex-center">
     <create-event-form @pushEvent="handlePushEvent($event)" :events="events" />
     <event-table  :events="events" />
+    <q-btn @click="handleEventFeedSubmit">Create Calendar</q-btn>
   </q-page>
 </template>
 
 <script setup>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import EventTable from "components/EventTable.vue";
 import CreateEventForm from "components/CreateEventForm.vue";
+import axios from "axios";
 
 defineComponent({name: "IndexPage"})
 
 function handlePushEvent(input) {
+  events.value.push(input)
   console.log("Pusheda", input)
 }
 
-const events = [
-]
+const events = ref([])
+
+function handleEventFeedSubmit() {
+  if(!events.value.length > 0) {
+    return
+  }
+  axios.post("http://localhost:8000/calgen/create-event-feed/", events.value)
+    .then((resp) => {
+      console.log("Rsp: ", resp)
+    })
+}
 </script>
