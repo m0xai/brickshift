784b7065df8fdab641785346fd7aa68e15283dad
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-11-09 00:37:01 +0100

    feat(frontend): create auth store with pinia

---
+++ frontend/src/boot/auth.store.js
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+import { ref } from "vue";
+
+export const useAuthStore = defineStore('auth', () => {
+  const token = ref("")
+  function setToken(input) {
+    token.value = input
+  }
+
+  return { token, setToken }
+},  {
+  persist: true
+})
