fd8fbf924401abc17e9ca0a0f97218bf1f99ca38
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-11-09 00:48:24 +0100

    feat(backend): include base urls into django app

--- backend/mysite/mysite/urls.py
+++ backend/mysite/mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include("base.urls")),
     path('calgen/', include("calgen.urls")),
     path('admin/', admin.site.urls),
 ]
