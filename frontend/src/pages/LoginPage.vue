371aa7fba82851381458f9d60fd45a1284a10037
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-11-08 22:48:54 +0100

    feat(frontend): create login page for authentication

---
+++ frontend/src/pages/LoginPage.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { defineComponent, ref } from "vue";
+
+defineComponent({name: "LoginPage"})
+const username = ref("")
+const password = ref("")
+
+// Add logic to implement user's login
+</script>
+
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <div class="flex flex-center">
+      <q-card class="login-wrapper">
+        <h2 class="text-h2">Login</h2>
+        <q-card-section>
+          <q-form>
+            <q-input v-model="username" label="Username" class="q-my-md" />
+            <q-input v-model="password" label="Password" class="q-my-md" />
+          </q-form>
+          <a href="#" class="q-my-md" >Password Vergessen?</a>
+          <q-card-actions class="q-my-lg">
+            <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+            <q-btn label="Submit" type="submit" color="primary"/>
+          </q-card-actions>
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-layout>
+</template>
+
+<style scoped>
+.login-wrapper {
+  width: 100%;
+  max-width: 400px;
+}
+</style>
