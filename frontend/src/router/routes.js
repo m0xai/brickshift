cb520a054e54e3daa3d94cec33afd03546a43b4c
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-11-09 15:26:48 +0100

    feat(frontend): add names to profile and user-management routes

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -13,19 +13,17 @@ const routes = [
     component: () => import("pages/LoginPage.vue")
   },
   {
-    name: "user-management",
     path: "/user-management",
     component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: "", component: () => import("pages/UserManagement.vue")}
+      { name: "user-management", path: "", component: () => import("pages/UserManagement.vue")}
     ]
   },
   {
-    name: "profile",
     path: "/profile",
     component: () => import("layouts/MainLayout.vue"),
     children: [
-      {path: "", component: () => import("pages/ProfilePage.vue")}
+      {name: "profile", path: "", component: () => import("pages/ProfilePage.vue")}
     ]
   },
   // Always leave this as last one,
