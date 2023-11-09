17781f10725e4bb039852388dcc30ee759198293
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-11-10 00:39:27 +0100

    quickfix(frontend): auth store path

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import { route } from 'quasar/wrappers'
 import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
 import routes from './routes'
-import { useAuthStore } from "../stores/auth";
+import { useAuthStore } from "stores/auth";
 
 /*
  * If not building with SSR mode, you can
