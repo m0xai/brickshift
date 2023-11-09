a27ff0f200f8cf54f6c51e2c7e914903fdf68e29
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-11-10 00:36:27 +0100

    feat(frontend): create logout button component

---
+++ frontend/src/components/LogoutBtn.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineComponent } from "vue";
+import { useRouter } from "vue-router";
+import { useAuthStore } from "stores/auth";
+
+defineComponent({name: "LogoutBtn"})
+
+const router = useRouter()
+const authStore = useAuthStore()
+function handleLogout() {
+  router.push("/#/login")
+  authStore.setToken("")
+}
+</script>
+
+<template>
+  <q-btn flat @click="handleLogout()">Logout</q-btn>
+</template>
