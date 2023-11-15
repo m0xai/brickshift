98dce392cf2ac5bbbabc49727e9c1766ae072db4
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-11-15 22:48:02 +0100

    feat(frontend): show actions and details columns in event table

--- frontend/src/components/EventTable.vue
+++ frontend/src/components/EventTable.vue
@@ -1,8 +1,10 @@
 <script setup>
 import axios from "axios";
 import { api } from "src/boot/axios";
+import EventEdit from "components/event/EventEdit.vue";
 
 const columns = [
+  {name: "details", label: "Details",  align: "center"},
   {
     name: "name",
     label: "Name",
@@ -13,7 +15,7 @@ const columns = [
   {name: "date", label: "Date", field: "date", align: "center"},
   {name: "start", label: "Start", field: "start", align: "center"},
   {name: "end", label: "End", field: "end", align: "center"},
-  { name: 'actions', label: 'Action(s)', align: "center"}
+  {name: 'actions', label: 'Action(s)', align: "center"}
 ]
 
 defineProps({
@@ -47,12 +49,32 @@ function handleEventDelete(event) {
             :columns="columns"
             :rows="events"
             row-key="name">
-              <template v-slot:body-cell-actions="props">
-                  <q-td :props="props">
-                    <q-btn icon="edit" @click="handleEventDelete(props.row)"></q-btn>
-                      <q-btn icon="delete" @click="handleEventDelete(props.row)"></q-btn>
-                  </q-td>
-              </template>
+            <template v-slot:body="props">
+              <q-tr :props="props">
+                <q-td key="details" auto-width>
+                  <q-btn size="sm" color="accent" round dense @click="props.expand = !props.expand" :icon="props.expand ? 'remove' : 'add'" />
+                </q-td>
+                <q-td
+                  v-for="col in props.cols.filter(v => v.name !== 'details' && v.name !== 'actions')"
+                  :key="col.name"
+                  :props="props"
+                >
+                  {{ col.value }}
+                </q-td>
+                <q-td key="actions" :props="props">
+                  <EventEdit :event-item="(props.row)" />
+                  <q-btn icon="delete" @click="handleEventDelete(props.row)"></q-btn>
+                </q-td>
+              </q-tr>
+              <q-tr v-show="props.expand" :props="props">
+                <q-td v-if="!props.row.description"  colspan="100%">
+                  <div class="text-center">No description provided.</div>
+                </q-td>
+                <q-td v-else colspan="100%">
+                  <div class="text-left">{{ props.row.description }}</div>
+                </q-td>
+              </q-tr>
+            </template>
           </q-table>
         </div>
     </q-card>
