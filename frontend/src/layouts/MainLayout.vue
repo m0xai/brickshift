dbbb93b23380c127567745785cc83076ef1d4c1e
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-11-16 13:35:10 +0100

    chore(frontend): apply code styles into MainLayout file

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,46 +1,49 @@
 <script setup>
-import { computed, onMounted, ref, watch } from 'vue'
+import { computed, onMounted, ref, watch } from "vue";
 import { useAuthStore } from "stores/auth";
-import useRouter from 'src/router';
-import { api } from 'src/boot/axios';
+import useRouter from "src/router";
+import { api } from "src/boot/axios";
 import LogoutBtn from "components/LogoutBtn.vue";
 import { useQuasar } from "quasar";
 import { useSettingsStore } from "stores/settings";
 
-const $q = useQuasar()
-const router = useRouter()
-const settingsStore = useSettingsStore()
+const $q = useQuasar();
+const router = useRouter();
+const settingsStore = useSettingsStore();
 
-const leftDrawerOpen = ref(false)
+const leftDrawerOpen = ref(false);
 function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+  leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 api.interceptors.request.use((config) => {
-  if(config.url.includes("login")) {
+  if (config.url.includes("login")) {
     return config;
   }
   const authStore = useAuthStore();
   config.headers.Authorization = "Token " + authStore.token;
-  return config
+  return config;
 });
 
 function toggleDarkMode() {
   settingsStore.toggle();
 }
 
-watch(() => settingsStore.isDark, () => {
-  $q.dark.toggle();
-});
+watch(
+  () => settingsStore.isDark,
+  () => {
+    $q.dark.toggle();
+  }
+);
 
 onMounted(() => {
-  if(settingsStore.isDark) {
-    $q.dark.set(true)
+  if (settingsStore.isDark) {
+    $q.dark.set(true);
   }
-})
+});
 
 const modeSwitchIcon = computed(() => {
-  return $q.dark.isActive ? "light_mode" : "dark_mode"
-})
+  return $q.dark.isActive ? "light_mode" : "dark_mode";
+});
 </script>
 
 <template>
@@ -56,32 +59,24 @@ const modeSwitchIcon = computed(() => {
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
-         BrickShift
-        </q-toolbar-title>
+        <q-toolbar-title> BrickShift </q-toolbar-title>
 
         <div>
           <LogoutBtn />
-          <q-btn @click="toggleDarkMode()" flat :icon="modeSwitchIcon" />
+          <q-btn
+            @click="toggleDarkMode()"
+            fab-mini
+            flat
+            :icon="modeSwitchIcon"
+          />
         </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-        <q-item
-          clickable
-          tag="a"
-        >
+        <q-item-label header> Essential Links </q-item-label>
+        <q-item clickable tag="a">
           <q-item-section>
             <q-item-label>Home</q-item-label>
           </q-item-section>
@@ -94,4 +89,3 @@ const modeSwitchIcon = computed(() => {
     </q-page-container>
   </q-layout>
 </template>
-
