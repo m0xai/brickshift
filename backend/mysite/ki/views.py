3dc815f2296122ab8a9632677f82bfce386782dd
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-12-03 22:09:39 +0100

    feat(backend): handle upload request and send vision ai image for prediction

--- backend/mysite/ki/views.py
+++ backend/mysite/ki/views.py
@@ -1,5 +1,9 @@
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+from django.conf import settings
 from django.http import HttpResponse
-from rest_framework import viewsets, mixins
+from msrest.authentication import ApiKeyCredentials
+from rest_framework import viewsets, mixins, serializers, status
+from rest_framework.response import Response
 
 from .models import WeekPlan
 from .serializers import WeekPlanSerializer
@@ -15,3 +19,21 @@ class WeekPlanViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.Ret
                       mixins.DestroyModelMixin, viewsets.GenericViewSet):
     queryset = WeekPlan.objects.all()
     serializer_class = WeekPlanSerializer
+
+
+    def send_ai_req(self, image):
+        credentials = ApiKeyCredentials(in_headers={"Prediction-key": settings.CV_PREDICTION_KEY})
+        predictor = CustomVisionPredictionClient(endpoint=settings.CV_PREDICTION_ENDPOINT, credentials=credentials)
+
+        results = predictor.classify_image(settings.CV_PROJECT_ID, settings.CV_MODEL_NAME, image.read())
+
+        # Display the results.
+        print(results)
+        prediction_results = list()
+        for prediction in results.predictions:
+            print(prediction.__dict__)
+            prediction_results.append({"tag_id": prediction.tag_id, "tag_name": prediction.tag_name,
+                                       "probability": "{0:.2f}%".format(prediction.probability * 100)})
+
+        return prediction_results
+
