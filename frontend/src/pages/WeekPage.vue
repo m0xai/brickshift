fedd125124e27b6f4bbd293c2b413fbd7e5d9ec7
Kerem Zopcuk <keremzopcuk@gmail.com>
2023-12-14 21:54:38 +0100

    feat(frontend): create WeekPage for showing single week instances

---
+++ frontend/src/pages/WeekPage.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineComponent, onMounted } from "vue";
+import WeekCard from "components/week/WeekCard.vue";
+import { useRoute } from "vue-router";
+
+defineComponent({ name: "WeekPage" });
+
+const route = useRoute();
+
+onMounted(() => {
+	console.log(route.params);
+});
+</script>
+
+<template>
+	<WeekCard />
+</template>
+
+<style scoped></style>
